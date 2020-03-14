98e6493b5eec25d10e1d60c2e1b6e7587594e937
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-03-14 12:04:19 +0100

    Replace dependency on rresult to a local definition of error_msgf to have few depedencies on emile.cmdliner

--- lib/emile_cmdliner.ml
+++ lib/emile_cmdliner.ml
@@ -1,8 +1,10 @@
 open Cmdliner
 
+let error_msgf fmt = Format.kasprintf (fun err -> Error (`Msg err)) fmt
+
 let mailbox =
   let parser x = match Emile.of_string x with
     | Ok v -> Ok v
-    | Error _ -> Rresult.R.error_msgf "%S is an invalid mail address" x in
+    | Error _ -> error_msgf "%S is an invalid mail address" x in
   let pp = Emile.pp_mailbox in
   Arg.conv ~docv:"<mailbox>" (parser, pp)
